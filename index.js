da467409cb50840ef1320243de0532c17d640533
pete <szajko.peter_2022b@bolyai-szakkozep.hu>
2025-11-17 18:27:39 +0100

    konzolra kiirjuk a stringeket

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("Teület Évszám Esemény");//Kiirom a  tablazat fejlecet a konzolra
+console.log("Amerika 1860-1865 polgárháború");//Kiirom a tablazat elso sorat a konzolra
+console.log("Európa 1914 június 28 merénylet szarajevóban");//Kiirom a tablazat masodik sorat a konzolra
+console.log("1914 július 28 A mohácsi csata");//Kiirom a tablazat harmadik sorat a konzolra
+console.log("Oroszország 1917 Oroszországi forradalom és bolsevik hatalomátvétel");//Kiirom a tablazat negyedik sorat a konzolra
+console.log("1933 Szovjetúnió megalakulása");//Kiirom a tablazat otodik sorat a konzolra    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
