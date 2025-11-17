551a6670893764b0cfc127550578dd82e5d2ef9b
pete <szajko.peter_2022b@bolyai-szakkozep.hu>
2025-11-17 19:06:00 +0100

    valtozok bevezetese

--- index.js
+++ index.js
@@ -1,10 +1,90 @@
-console.log("Teület Évszám Esemény");//Kiirom a  tablazat fejlecet a konzolra
-console.log("Amerika 1860-1865 polgárháború");//Kiirom a tablazat elso sorat a konzolra
-console.log("Európa 1914 június 28 merénylet szarajevóban");//Kiirom a tablazat masodik sorat a konzolra
-console.log("1914 július 28 A mohácsi csata");//Kiirom a tablazat harmadik sorat a konzolra
-console.log("Oroszország 1917 Oroszországi forradalom és bolsevik hatalomátvétel");//Kiirom a tablazat negyedik sorat a konzolra
-console.log("1933 Szovjetúnió megalakulása");//Kiirom a tablazat otodik sorat a konzolra    
+/**
+ * @type {string} string valtozo amiben a tablazat fejlecenek elso eleme van
+ */
+const thfirstitem="Terület"; //th elso eleme
+/**
+ * @type {string} string valtozo amiben a tablazat fejlecenek masdik eleme van
+ */
+const thseconditem="Évszám"; //th masodik eleme
+/**
+ * @type {string} string valtozo amiben a tablazat fejlecenek harmadik eleme van
+ */
+const ththirditem="Esemény"; //th harmadik eleme
 
+/**
+ * @type {string} string valtozo amiben a tablazat elso soranak elso eleme van
+ */
+const firsttrfirstitem="Amerika"; //elso sor elso eleme
+/**
+ * @type {string} string valtozo amiben a tablazat elso soranak masodik eleme van
+ */
+const firsttrseconditem="1860-1865"; //elso sor masodik eleme
+/**
+ * @type {string} string valtozo amiben a tablazat elso soranak harmadik eleme van
+ */
+const firsttrthirditem="polgárháború"; //elso sor harmadik eleme
+
+/**
+ * @type {string} string valtozo amiben a tablazat masodik soranak elso eleme van
+ */
+const secondtrfirstitem="Európa"; //masodik sor elso eleme
+/**
+ * @type {string} string valtozo amiben a tablazat masodik soranak masodik eleme van
+ */
+const secondtrseconditem="1914 június 28"; //masodik sor masodik eleme
+/**
+ * @type {string} string valtozo amiben a tablazat masodik soranak harmadik eleme van
+ */
+const secondtrthirditem="merénylet szarajevóban"; //masodik sor harmadik eleme
+/**
+ * @type {string} string valtozo amiben a tablazat masodik soranak negyedik eleme van
+ */
+const secondtrfourthitem="1526 július 29"; //masodik sor negyedik eleme
+/**
+ * @type {string} string valtozo amiben a tablazat masodik soranak otodik eleme van
+ */
+const secondtrfifthitem="A mohácsi csata"; //masodik sor otodik eleme
+
+/**
+ * @type {string} string valtozo amiben a tablazat harmadik soranak elso eleme van
+ */
+const thirdtrfirstitem="Oroszország"; //harmadik sor elso eleme
+/**
+ * @type {string} string valtozo amiben a tablazat harmadik soranak masodik eleme van
+ */
+const thirdtrseconditem="1917"; //harmadik sor masodik eleme
+/**
+ * @type {string} string valtozo amiben a tablazat harmadik soranak harmadik eleme van
+ */
+const thirdtrthirditem="Oroszországi forradalom és bolsevik hatalomátvétel"; //harmadik sor harmadik eleme
+/**
+ * @type {string} string valtozo amiben a tablazat harmadik soranak negyedik eleme van
+ */
+const thirdtrfourthitem="1933"; //harmadik sor negyedik eleme 
+/**
+ * @type {string} string valtozo amiben a tablazat harmadik soranak otodik eleme van
+ */
+const thirdtrfifthitem="Szovjetúnió megalakulása"; //harmadik sor otodik eleme
+
+console.log(thfirstitem) //kiirom a konzolra a th elso valtozojat
+console.log(thseconditem) //kiirom a konzolra a th masodik valtozojat
+console.log(ththirditem) //kiirom a konzolra a th harmadik valtozojat
+
+console.log(firsttrfirstitem) //kiirom a konzolra az elso sor elso valtozojat
+console.log(firsttrseconditem) //kiirom a konzolra az elso sor masodik valtozojat
+console.log(firsttrthirditem) //kiirom a konzolra az elso sor harmadik valtozojat
+
+console.log(secondtrfirstitem) //kiirom a konzolra a masodik sor elso valtozojat
+console.log(secondtrseconditem) //kiirom a konzolra a masodik sor masodik valtozojat
+console.log(secondtrthirditem) //kiirom a konzolra a masodik sor harmadik valtozojat
+console.log(secondtrfourthitem) //kiirom a konzolra a masodik sor negyedik valtozojat
+console.log(secondtrfifthitem) //kiirom a konzolra a masodik sor otodik valtozojat
+
+console.log(thirdtrfirstitem) //kiirom a konzolra a harmadik sor elso valtozojat    
+console.log(thirdtrseconditem) //kiirom a konzolra a harmadik sor masodik valtozojat
+console.log(thirdtrthirditem) //kiirom a konzolra a harmadik sor harmadik valtozojat
+console.log(thirdtrfourthitem) //kiirom a konzolra a harmadik sor negyedik valtozojat
+console.log(thirdtrfifthitem) //kiirom a konzolra a harmadik sor otodik valtozojat
 
 
 
